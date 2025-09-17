432af5183fae923ca8550968a6b96883157f3402
Brenoz001 <brenoxgomes0912@gmail.com>
2025-09-17 13:43:40 -0300

    Definindo variaveis do ambiente

--- teste.py
+++ teste.py
@@ -5,7 +5,10 @@ load_dotenv()
 
 url = os.getenv("URL")
 name = os.getenv("DB_NAME")
-ok = "ok"
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
 
 print(url)
 print(name)
