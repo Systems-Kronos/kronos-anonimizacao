efaba3ad0813f2c4f1f47460eecccc84be07fbb4
Gabriel Teixeira Vigna <gabrielvigna-ieg@instituto.ad>
2025-09-19 11:38:11 -0300

    conexao bd

--- conexao.py
+++ conexao.py
@@ -1,23 +1,26 @@
-import os
 from dotenv import load_dotenv
+import os
 import psycopg2 as pg
 from psycopg2 import Error
-load_dotenv()
-def conn():   
+
+def conn():
     try:
         conecta = pg.connect(
-            user = os.getenv("USER"),
-            password = os.getenv("PASSWORD"),
-            host = os.getenv("DB_HOST"),
-            port = os.getenv("DB_PORT"),
-            database = os.getenv("DB_NAME")
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            database=os.getenv("DB_NAME")
         )
-        print("Conexão bem sucedida.")
+    
+        db_name = conecta.get_dsn_parameters()['dbname']
+        print(f"Conexão bem sucedida ao banco de dados: {db_name}")
         return conecta
-    except Error as e :
+    except Error as e:
         print(f"Erro ao conectar com o banco de dados: {e}")
 
 def desconecta(conecta):
     if conecta:
         conecta.close()
         print("Conexão encerrada.")
+conn()
