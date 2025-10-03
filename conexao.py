e8ccc3f0beb8f5989cfb0d18f37ec25c3914332b
Gabriel Teixeira Vigna <gabrielvigna-ieg@instituto.ad>
2025-10-03 09:02:20 -0300

    Versão com todas as funções

--- conexao.py
+++ conexao.py
@@ -3,24 +3,25 @@ import os
 import psycopg2 as pg
 from psycopg2 import Error
 
+load_dotenv()
+
 def conn():
-    try:
+    try: 
         conecta = pg.connect(
+            dbname=os.getenv("DB_NAME"),  # Corrigido de db_name para dbname
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            database=os.getenv("DB_NAME")
+            port=os.getenv("DB_PORT")
         )
-    
-        db_name = conecta.get_dsn_parameters()['dbname']
+        db_name = conecta.get_dsn_parameters()['dbname']  # Corrigido para 'dbname'
         print(f"Conexão bem sucedida ao banco de dados: {db_name}")
         return conecta
     except Error as e:
-        print(f"Erro ao conectar com o banco de dados: {e}")
+        print(f"Erro ao 00conectar com o banco de dados: {e}")
+        return None  # Retornar None explicitamente em caso de erro
 
 def desconecta(conecta):
     if conecta:
         conecta.close()
         print("Conexão encerrada.")
-conn()
